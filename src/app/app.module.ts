a619cde56443447f7356be97ab03e65295c72cf2
Roberto Agirre <r.agirre@ibermatica.com>
2019-12-17 16:50:16 +0100

    Fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragComponent } from './components/drag/drag.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PaisesComponent } from './components/paises/paises.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
